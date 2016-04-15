5292114c1dcb9f0b977e03c4b24920030055cea6
AMDraisma <tsobutter@gmail.com>
2016-04-15 12:07:31 +0200

    Added base gui

--- src/Organictable.java
+++ src/Organictable.java
@@ -1,11 +1,12 @@
+import javax.swing.*;
 
-
-public class Organictable {
+public class Organictable extends JFrame {
     public static void main(String[] args) {
-
+        new Organictable();
     }
 
-    public Organictable() {
-
+    public Organictable(){
+        this.setSize(600,300);
+        this.setVisible(true);
     }
 }
