30dfa64822e3a2a802c0ad1ffc29074228b45177
AMDraisma <tsobutter@gmail.com>
2016-04-22 10:34:12 +0200

    Added differentiation of radio buttons

--- src/Organictable.java
+++ src/Organictable.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class Organictable extends JFrame {
+
+    private FilePanel filepanel;
+    private ButtonGroup group;
 
     public static void main(String[] args) {
         new Organictable();
@@ -14,14 +17,22 @@ public class Organictable extends JFrame {
         this.setSize(600,300);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(new FilePanel());
+        filepanel = new FilePanel();
+        this.setContentPane(filepanel);
         this.pack();
     }
 
+    public FilePanel getFilepanel() {
+        return filepanel;
+    }
+
     private class FilePanel extends JPanel {
+
+        private FilePanelHandler filepanelhandler;
+
         public FilePanel() {
             this.setLayout(new GridBagLayout());
-
+            this.filepanelhandler = new FilePanelHandler();
 
             GridBagConstraints c = new GridBagConstraints();
             c.weightx=1;
@@ -29,6 +40,8 @@ public class Organictable extends JFrame {
             c.fill= GridBagConstraints.BOTH;
 
             JRadioButton fastaButton = new JRadioButton("Fasta file");
+            fastaButton.setActionCommand("fasta");
+            fastaButton.setSelected(true);
             c.gridwidth=1;
             c.gridheight=1;
             c.gridx=0;
@@ -36,6 +49,7 @@ public class Organictable extends JFrame {
             this.add(fastaButton, c);
 
             JRadioButton groupButton = new JRadioButton("Orthomcl file");
+            groupButton.setActionCommand("orthomcl");
             c.gridwidth=1;
             c.gridheight=1;
             c.gridx=1;
@@ -43,6 +57,7 @@ public class Organictable extends JFrame {
             this.add(groupButton, c);
 
             JRadioButton wolfButton = new JRadioButton("WoLF PSORT file");
+            wolfButton.setActionCommand("wolfpsort");
             c.gridwidth=1;
             c.gridheight=1;
             c.gridx=2;
@@ -50,6 +65,7 @@ public class Organictable extends JFrame {
             this.add(wolfButton, c);
 
             JRadioButton signalButton = new JRadioButton("SignalP file");
+            signalButton.setActionCommand("signalp");
             c.gridwidth=1;
             c.gridheight=1;
             c.gridx=0;
@@ -57,6 +73,7 @@ public class Organictable extends JFrame {
             this.add(signalButton, c);
 
             JRadioButton hmmerButton = new JRadioButton("HMMER file");
+            hmmerButton.setActionCommand("hmmer");
             c.gridwidth=1;
             c.gridheight=1;
             c.gridx=1;
@@ -64,19 +81,41 @@ public class Organictable extends JFrame {
             this.add(hmmerButton, c);
 
             JButton openButton = new JButton("Open file");
-            openButton.setSelected(true);
+            openButton.addActionListener(filepanelhandler);
             c.gridwidth=3;
             c.gridheight=1;
             c.gridx=0;
             c.gridy=2;
             this.add(openButton, c);
 
-            ButtonGroup group = new ButtonGroup();
+            group = new ButtonGroup();
             group.add(fastaButton);
             group.add(groupButton);
             group.add(wolfButton);
             group.add(signalButton);
             group.add(hmmerButton);
+        }
+
+        public String getSelectedRadioButtonActionCommand(){
+            return group.getSelection().getActionCommand();
+        }
+    }
+
+    class FilePanelHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            String ac = getFilepanel().getSelectedRadioButtonActionCommand();
+            if (Objects.equals(ac, "fasta")) {
+                // todo open fasta file and parse it
+            }if (Objects.equals(ac, "orthomcl")) {
+                // todo open orthomcl file and parse it
+            }if (Objects.equals(ac, "wolfpsort")) {
+                // todo open wolfpsort file and parse it
+            }if (Objects.equals(ac, "signalp")) {
+                // todo open signalp file and parse it
+            }if (Objects.equals(ac, "hmmer")) {
+                // todo open hmmer file and parse it
+            }
         }
     }
 }
